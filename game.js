01bb0010b4876c18f715f8115f54c66531fac68b
kenco <ysa2780@naver.com>
2024-11-15 19:41:20 +0900

    [A key press Game Start]
    A key를 누르면 게임이 시작되는 로직을 구현했습니다.

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 var gamePattern = [];
 var userClickedPattern = [];
 const buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var start = false;
+
 $(".btn").on("click", function() {
     var userChosenColour = $(this).attr("id");
     playSound(userChosenColour);
@@ -8,18 +11,29 @@ $(".btn").on("click", function() {
     userClickedPattern.push(userChosenColour);
 });
 
+$(document).keydown(function(e){
+    if(e.keyCode == 65 && start == false) {
+        nextSequence();
+        start = true;
+    }
+})
+
 function playSound(name) {
     var audio = new Audio(`./sounds/${name}.mp3`);
     audio.play();
 };
 
 function nextSequence() {
+    level ++;
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+
+    
+    $("#level-title").text(`level ${level}`);
 };
 
 function animatePress(currentColour) {
