b126727f76005418d5af58b446b15a2c3db44843
kenco <ysa2780@naver.com>
2024-11-15 19:40:39 +0900

    [UserClickEvent]
    User가 color box를 클릭했을 때, 반짝이는 이벤트가 생기도록 css를 사용해 애니메이션을 추가했습니다.

--- game.js
+++ game.js
@@ -1,11 +1,17 @@
 var gamePattern = [];
 var userClickedPattern = [];
 const buttonColours = ["red", "blue", "green", "yellow"];
+$(".btn").on("click", function() {
+    var userChosenColour = $(this).attr("id");
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+});
 
 function playSound(name) {
     var audio = new Audio(`./sounds/${name}.mp3`);
     audio.play();
-}
+};
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
@@ -16,10 +22,12 @@ function nextSequence() {
     playSound(randomChosenColour);
 };
 
-$(".btn").on("click", function() {
-    var userChosenColour = $(this).attr("id");
-    playSound(userChosenColour);
-    userClickedPattern.push(userChosenColour);
-    console.log(userChosenColour);
-})
+function animatePress(currentColour) {
+    var delayInMilliseconds = 100;
+    $(`#${currentColour}`).addClass("pressed");
+
+    setTimeout(function() {
+        $(`#${currentColour}`).removeClass("pressed");
+    },delayInMilliseconds);
+};
 
